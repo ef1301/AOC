ad56112c838f8c2af48c3b9a3b087812dd0b9682
ef1301 <fangemily60@gmail.com>
2018-12-01 13:45:02 -0500

    done

--- day_01/chronal_cali.py
+++ day_01/chronal_cali.py
@@ -1,22 +1,14 @@
+f = open("/home/fang/AOC/day_01/input.txt")
+read = f.read()
+def makelist(input):
+    list = ""
+    for item in input:
+        list = list + item
+    return str(list)
+
+print(makelist(read))
+
 def freq(list):
-    """
-Frequency drift detected. Cannot maintain destination lock." Below the message, the device shows a sequence of changes in frequency (your puzzle input). A value like +6 means the current frequency increases by 6; a value like -3 means the current frequency decreases by 3.
-
-For example, if the device displays frequency changes of +1, -2, +3, +1, then starting from a frequency of zero, the following changes would occur:
-
-Current frequency  0, change of +1; resulting frequency  1.
-Current frequency  1, change of -2; resulting frequency -1.
-Current frequency -1, change of +3; resulting frequency  2.
-Current frequency  2, change of +1; resulting frequency  3.
-In this example, the resulting frequency is 3.
-
-Here are other example situations:
-
-+1, +1, +1 results in  3
-+1, +1, -2 results in  0
--1, -2, -3 results in -6
-Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
-"""
     start = 0
     split = list.split()
     for item in split:
@@ -35,7 +27,4 @@ Starting with a frequency of zero, what is the resulting frequency after all of 
             num = int(strip)
             start = start + num
     return start
-##    return split
-
-print(freq("+1, -2, +3, +1"))
-        
+print(freq(makelist(read)))
