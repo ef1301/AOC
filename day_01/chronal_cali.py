1bebf17727bbee486aaf97300863656997fe4221
ef1301 <fangemily60@gmail.com>
2018-12-01 14:02:37 -0500

    part 2

--- day_01/chronal_cali.py
+++ day_01/chronal_cali.py
@@ -8,8 +8,30 @@ def makelist(input):
 
 print(makelist(read))
 
+##def freq(list):
+##    start = 0
+##    split = list.split()
+##    for item in split:
+##        if "-" in item:
+####            print(item)
+##            strip = item.strip("-,")
+####            print(strip)
+####            print(type(strip))
+##            num = int(strip)
+##            start = start - num
+##        else:
+####            print(item)
+##            strip = item.strip("+,")
+####            print(strip)
+####            print(type(strip))
+##            num = int(strip)
+##            start = start + num
+##    return start
+##print(freq(makelist(read)))
+
 def freq(list):
     start = 0
+    results = {}
     split = list.split()
     for item in split:
         if "-" in item:
@@ -19,6 +41,8 @@ def freq(list):
 ##            print(type(strip))
             num = int(strip)
             start = start - num
+            results.setdefault(start,0)
+            results[start] += 1
         else:
 ##            print(item)
             strip = item.strip("+,")
@@ -26,5 +50,15 @@ def freq(list):
 ##            print(type(strip))
             num = int(strip)
             start = start + num
-    return start
+            results.setdefault(start,0)
+            results[start] += 1
+    return results
+
+def twice(dict):
+    for item in dict:
+        if dict[item] == 2:
+            twice = item
+            return twice
 print(freq(makelist(read)))
+print(twice(freq(makelist(read))))
+
