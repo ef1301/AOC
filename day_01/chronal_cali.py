fd9408a0d021695054dc4edafbf9893c1c5c71f4
ef1301 <fangemily60@gmail.com>
2018-12-01 20:50:44 -0500

    slow

--- day_01/chronal_cali.py
+++ day_01/chronal_cali.py
@@ -1,3 +1,4 @@
+
 f = open("/home/fang/AOC/day_01/input.txt")
 
 def freq(dict):
@@ -6,19 +7,16 @@ def freq(dict):
         start = start + int(item)
         dict.setdefault(start,0)
         dict[start] += 1
-        while True:
-            if dict[start] == 2:
-                return start
-            else:
-                freq(dict)
     return dict
 
-##def check(dict):
-##    while True:
-##        if dict[key] == 2:
-##            return key
-##        else:
-##            freq(dict)
+def check(dict):
+    while True:
+        for keys in dict:
+            if dict[keys] == 2:
+                return key
+            else:
+                freq(dict)
 
 print(freq({}))
-##print(check(freq({})))
+print(check(freq({})))
+
