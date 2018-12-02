664ed2790047bc464a4fd584688fac4fb5b72541
ef1301 <fangemily60@gmail.com>
2018-12-01 20:36:50 -0500

    loop needs fixing

--- day_01/chronal_cali.py
+++ day_01/chronal_cali.py
@@ -1,36 +1,24 @@
 f = open("/home/fang/AOC/day_01/input.txt")
-read = f.read()
-def makelist(input):
-    list = ""
-    for item in input:
-        list = list + item
-    return str(list)
 
-s = makelist(read)
+def freq(dict):
+    start = 0
+    for item in f:
+        start = start + int(item)
+        dict.setdefault(start,0)
+        dict[start] += 1
+        while True:
+            if dict[start] == 2:
+                return start
+            else:
+                freq(dict)
+    return dict
 
-def freq(list,start):
-    results = {}
-    split = list.split()
-    for item in split:
-        if "-" in item:
-            strip = item.strip("-")
-            start = start - int(strip)
-            results.setdefault(start,0)
-            results[start] += 1
-        else:
-            strip = item.strip("+")
-            start = start + int(strip)
-            results.setdefault(start,0)
-            results[start] += 1
-    return results
+##def check(dict):
+##    while True:
+##        if dict[key] == 2:
+##            return key
+##        else:
+##            freq(dict)
 
-
-def check(results):
-    for item in results:
-        if results[item] == 2:
-            return True
-        else:
-            return "sad"
-    
-f = freq(s, 0)
-print(check(f))
+print(freq({}))
+##print(check(freq({})))
